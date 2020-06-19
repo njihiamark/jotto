2004c781a3ed0db8dd0852215d456c09e055d31f
njihiamark <njihiamark@gmail.com>
2020-06-19 17:20:25 +0300

    Add proptypes for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` props is null)
  */
-export default (props) => {
+const Congrats = (props) => {
     if(props.success){
         return(
             <div data-test="component-congrats">
@@ -21,4 +21,10 @@ export default (props) => {
             <div data-test="component-congrats"/>  
         );
     } 
-}
+};
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
+};
+
+export default Congrats;
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,9 +1,21 @@
+import checkPropTypes from "check-prop-types";
+
 /**
  * Returns node(s) with given data-test attribute.
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper. 
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
  * @param {String} val - Value of data-test attribute for search.
- * @returns {ShallowWrapper} 
+ * @returns {ShallowWrapper}
  */
 export const findByTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test="${val}"]`);
-}
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefined();
+};
