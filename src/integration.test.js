72977f22ea084b4aa0e273681c69f4814c8b570d
njihiamark <njihiamark@gmail.com>
2020-06-26 18:04:34 +0300

    set up guessWords Tests

---
+++ src/integration.test.js
@@ -0,0 +1,22 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guess action dispatcher', () => {
+    describe('no guessed words', ()=>{
+        test('updates state correctly for unsuccesful guess', () => {
+
+        });
+        test('updates state correctly for succesful guess', () => {
+
+        });
+    });
+
+    describe('some guessed words', ()=>{
+        test('updates state correctly for unsuccesful guess', () => {
+
+        });
+        test('updates state correctly for succesful guess', () => {
+
+        });
+    });
+});
